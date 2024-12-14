aaf1dae303307bd17191dc01ceb93f6e7cce7f92
taufiq <taufiqseptry17@gmail.com>
2024-12-14 17:11:22 +0700

    refactor: update application configuration to use UUIDs for primary key

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,11 @@ module SleepTracker
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Configuration for setting the orm to use UUIDs for primary keys.
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+    
   end
 end
