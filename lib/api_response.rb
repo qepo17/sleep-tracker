ce75c11fcf9e39d07bc4ceebd8732361a3eb6309
taufiq <taufiqseptry17@gmail.com>
2024-12-14 20:24:54 +0700

    feat: module ApiResponse to standardize json api response

---
+++ lib/api_response.rb
@@ -0,0 +1,15 @@
+module ApiResponse
+  extend ActiveSupport::Concern
+
+  def error_response(body, status = 400)
+    render json: { error: body }, status: status
+  end
+
+  def unauthorized_response
+    render json: { error: "Please log in to access this resource" }, status: :unauthorized
+  end
+
+  def success_response(body, status = 200)
+    render json: body, status: status
+  end
+end
