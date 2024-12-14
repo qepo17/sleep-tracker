7123f5eda1d075409b83e8096085609cb8eb71d0
taufiq <taufiqseptry17@gmail.com>
2024-12-14 20:26:12 +0700

    refactor: implement standardize api response to get sleep_records endpoint

--- app/controllers/sleep_records_controller.rb
+++ app/controllers/sleep_records_controller.rb
@@ -2,12 +2,12 @@ class SleepRecordsController < ApplicationController
   def index
     current_user_id = session[:current_user_id]
 
-    return render json: { error: "Not logged in" }, status: :unauthorized unless current_user_id
+    return unauthorized_response unless current_user_id
 
-    sleep_records = SleepRecord.find_by(user_id: current_user_id)
+    sleep_records = SleepRecord.find_by(user_id: 1)
 
-    return render json: { error: "No sleep records found" }, status: :not_found if sleep_records.nil?
+    return error_response("No sleep records found", 404) if sleep_records.nil? || sleep_records.empty?
 
-    render json: sleep_records
+    render success_response(sleep_records, :created)
   end
 end
